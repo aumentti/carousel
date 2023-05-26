ee9573b768faf1aa8c03f8ef7fbad5df4d0a4cd1
Huffer <hugoferreirait@gmail.com>
2023-05-26 16:05:01 -0300

    fixes

--- carousel.js
+++ carousel.js
@@ -6,33 +6,32 @@ const items = document.getElementsByClassName("carousel-item");
 const prevButton = document.getElementById("carousel-prev-btn");
 const nextButton = document.getElementById("carousel-next-btn");
 
-const totalItems = items.length - 1;
+const totalItems = items.length;
 const itemWidth = items[0].clientWidth;
 
-let currIdx = 0;
+let currIdx = 1;
 
 carouselContainer.style.width = `${itemWidth}px`;
 prevButton.style.display = "none";
 
 function hideOrShowButton() {
-  if (currIdx === totalItems) {
-    nextButton.style.display = "none";
-    return;
-  }
-
-  if (currIdx === 0) {
-    prevButton.style.display = "none";
-    return;
-  }
   nextButton.style.display = "block";
   prevButton.style.display = "block";
+
+  if (currIdx === totalItems) {
+    nextButton.style.display = "none";
+  }
+
+  if (currIdx === 1) {
+    prevButton.style.display = "none";
+  }
 }
 
 function showPreviousSlide() {
   currIdx = currIdx-- === 0 ? 0 : currIdx--;
   hideOrShowButton();
 
-  const translate = `-${itemWidth * (currIdx + 1) - itemWidth}`;
+  const translate = `-${itemWidth * (currIdx - itemWidth)}`;
 
   carousel.style.transform = `translateX(${translate}px)`;
 }
@@ -40,8 +39,7 @@ function showPreviousSlide() {
 function showNextSlide() {
   currIdx = currIdx++ >= totalItems ? totalItems : currIdx++;
   hideOrShowButton();
-
-  carousel.style.transform = `translateX(-${itemWidth * currIdx}px)`;
+  carousel.style.transform = `translateX(-${itemWidth * (currIdx - 1)}px)`;
 }
 
 prevButton.addEventListener("click", showPreviousSlide);
